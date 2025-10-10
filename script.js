9889a440b69cd9033b1cbaaf0f1fa8c008444108
Nazreen998 <nazreenbegam1998@gmail.com>
2025-10-10 16:44:27 +0530

    modified js file

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-<<<<<<< HEAD
-// Infinite scroll duplication
-const track = document.querySelector('.slider-track');
-const clone = track.innerHTML;
-track.innerHTML += clone;
-=======
-// Infinite scroll duplication
-const track = document.querySelector('.slider-track');
-const clone = track.innerHTML;
-track.innerHTML += clone;
->>>>>>> 71f6dffbeb7b49aa74c8e42b564a0ddaad05f2be
+function initMap() {
+  const location = { lat: 9.8489, lng: 78.0869 }; // your coordinates
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: location,
+  });
+
+  const customIcon = {
+    url: "logo.png", // 🔹 your image file here
+    scaledSize: new google.maps.Size(60, 60), // resize as needed
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(30, 60),
+  };
+
+  new google.maps.Marker({
+    position: location,
+    map: map,
+    icon: customIcon,
+  });
+}
